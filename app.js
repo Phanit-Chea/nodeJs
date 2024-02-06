45db6ff59f725299e579c42a36cdece560b93af8
Phanit Chea <phanit.chea@student.passerellesnumeriques.org>
2024-02-06 16:38:25 +0700

    add remote

---
+++ app.js
@@ -0,0 +1,20 @@
+const logger = require('node-color-log');
+// logger.warn("Sucess");
+
+// const color = 'red';
+// const message = 'any string you will put into console.log';
+// const setting = {
+//     bold: true,
+//     dim: true,
+//     underscore: true,
+//     reverse: true,
+//     italic: true,
+//     strikethrough: true
+// }
+
+// logger.colorLog({
+//     font: 'black',
+//     bg: 'yellow'
+// },message, setting);
+
+
