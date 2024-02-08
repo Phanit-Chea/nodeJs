907e16bfea078f01a1916f9ecfec193f518c28aa
Phanit Chea <phanit.chea@student.passerellesnumeriques.org>
2024-02-08 20:18:21 +0700

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
 const logger = require('node-color-log');
-// logger.warn("Sucess");
-
-// const color = 'red';
-// const message = 'any string you will put into console.log';
-// const setting = {
-//     bold: true,
-//     dim: true,
-//     underscore: true,
-//     reverse: true,
-//     italic: true,
-//     strikethrough: true
-// }
-
-// logger.colorLog({
-//     font: 'black',
-//     bg: 'yellow'
-// },message, setting);
+console.log("Hello")
 
 
